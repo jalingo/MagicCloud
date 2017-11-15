b93bf609b3073838fbdfea2345816b8124e36b83
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-15 13:20:12 -0800

    minor cleanup

--- MagicCloud/Strategies/*PrepareFrom.swift
+++ MagicCloud/Strategies/*PrepareFrom.swift
@@ -12,19 +12,10 @@ import CloudKit
 func prepare<T: Recordable>(type: T.Type, from record: CKRecord/*, in db: CKDatabase*/) -> Recordable {
     var recordable: Recordable = T()
     
-//    switch type {
-//    case MockReferable().recordType: recordable = MockReferable()
-//    default: recordable = MockRecordable()
-//    }
-
-    // TODO: Need to make generic or won't be compatible for non-mocks without coupling...
-//    recordable =
-    
     // Overwrites default recordable with data from recovered record.
     // Order of these two lines is important.
     recordable.recordID = record.recordID
     for (key, _) in recordable.recordFields { recordable.recordFields[key] = record[key] }
-//    recordable.database = db
     
     return recordable
 }
