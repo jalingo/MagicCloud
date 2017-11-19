546914801563ea4c15df8f323c48d2f4a1b86dce
Hayley McCrory <hayleymccrory@Hayley-McCrorys-MacBook-Air.local>
2017-11-19 13:40:09 -0800

    moved tests in

--- MagicCloudTests/Protocols/RecieverTests.swift
+++ MagicCloudTests/Protocols/RecieverTests.swift
@@ -23,17 +23,68 @@ class RecievesRecTests: XCTestCase {
     
     func testReceiverHasAssociatedTypeRecordable() { XCTAssert(mock?.recordables is [Recordable]) }
     
+    func testVoteReceiverCanStartListening() {
+        let expect = expectation(description: "Receiver Heard Notification")
+        var passed = false
+        
+        mock?.startListening() {
+            passed = true
+            expect.fulfill()
+        }
+        
+        NotificationCenter.default.post(name: Notification.Name(mock!.notifyCreated), object: nil)
+        
+        wait(for: [expect], timeout: 2)
+        XCTAssert(passed)
+    }
+    
+    func testVoteReceiverCanStopListening() {
+        var passed = true
+        
+        mock?.startListening() { passed = false }
+        mock?.stopListening()
+        
+        NotificationCenter.default.post(name: Notification.Name(mock!.notifyUpdated), object: nil)
+        XCTAssert(passed)
+    }
+    
+    func testVoteReceiverCanDownloadAll() {
+        let _ = prepareDatabase()
+        
+        let expect = expectation(description: "All Votes Downloaded")
+        mock?.download() { expect.fulfill() }
+        wait(for: [expect], timeout: 3)
+        
+        let allVotes = testVotes()
+        if let votes = mock?.recordables {
+            XCTAssertEqual(allVotes, votes)
+        } else {
+            XCTFail()
+        }
+        
+        let _ = cleanUpDatabase()
+    }
+    
+    func testVoteReceiverDownloadsFromListening() {
+        let _ = prepareDatabase()
+        
+        mock?.startListening()  // <-- At this point empty
+        NotificationCenter.default.post(name: Notification.Name(mock!.notifyUpdated), object: nil)
+        
+        XCTAssert(mock?.recordables.count != 0)
+        
+        let _ = cleanUpDatabase()
+    }
+    
     // MARK: - Functions: XCTestCase
     
     override func setUp() {
         super.setUp()
-
         mock = MockReceiver()
     }
     
     override func tearDown() {
         mock = nil
-        
         super.tearDown()
     }
 }
