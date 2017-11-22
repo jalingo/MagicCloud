56d205828629bd2e4754bf56f40280a0c7fa60e4
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-22 11:59:24 -0800

    Added Subscriber

--- MagicCloud/Strategies/ListenFor.swift
+++ MagicCloud/Strategies/ListenFor.swift
@@ -9,79 +9,70 @@
 import CloudKit
 import UserNotifications
 
-/**
-    This strategy creates a subscription that listens for injected change of record type at database and allows consequence.
- 
-    - Note: CKNotificationInfo is standardized, but includes a CKRecordID with push notification.
- 
-    - Parameters:
-        - for: CKRecord.recordType that subscription listens for changes with.
-        - change: CKQuerySubscriptionOptions for subscription.
-        - database: DatabaseType for subscription to be saved to.
-        - count: Number of retries left for error handling (cannot be set higher than 3).
-        - followUp: Completion Block that executes after subscription saved to the database.
- */
-public func setupListener(for type: String,
-                          change trigger: CKQuerySubscriptionOptions,
-                          at database: DatabaseType = .publicDB,
-                          withTries count: Int = 2,
-                          consequence followUp: OptionalClosure = nil) -> String {
-    let left: Int
-    count < 2 ? (left = count - 1) : (left = 2)
+public class Subscriber {
+
+    // MARK: - Properties
     
-    // Create subscription
-    let predicate = NSPredicate(value: true)
-    let subsciption = CKQuerySubscription(recordType: type, predicate: predicate, options: trigger)
-    subsciption.notificationInfo = CKNotificationInfo()
+    var id: String?
     
-    // Saves the subscription to database
-    database.db.save(subsciption) { _, possibleError in
+    // MARK: - Functions
+    
+    /**
+     This strategy creates a subscription that listens for injected change of record type at database and allows consequence.
+     
+     - Note: CKNotificationInfo is standardized, but includes a CKRecordID with push notification.
+     
+     - Parameters:
+     - for: CKRecord.recordType that subscription listens for changes with.
+     - change: CKQuerySubscriptionOptions for subscription.
+     - database: DatabaseType for subscription to be saved to.
+     - count: Number of retries left for error handling (cannot be set higher than 3).
+     - followUp: Completion Block that executes after subscription saved to the database.
+     */
+    func start(for type: String, change trigger: CKQuerySubscriptionOptions, at database: DatabaseType = .publicDB, consequence followUp: OptionalClosure = nil) {
+        
+        // Create subscription
+        let predicate = NSPredicate(value: true)
+        let subsciption = CKQuerySubscription(recordType: type, predicate: predicate, options: trigger)
+        subsciption.notificationInfo = CKNotificationInfo()
+        
+        // Saves the subscription to database
+        database.db.save(subsciption) { _, possibleError in
 print("** listener save operation completing")
-        if let error = possibleError as? CKError {
+            if let error = possibleError as? CKError {
 print("** not successful \(error.code.rawValue) - \(error)")
-            
-            guard error.code != CKError.Code.serverRejectedRequest else {
-                disableListener(subscriptionID: subsciption.subscriptionID, at: database)
-                return
+                
+                guard error.code != CKError.Code.serverRejectedRequest else {
+                    self.end(subscriptionID: subsciption.subscriptionID, at: database)
+                    self.start(for: type,
+                                           change: trigger,
+                                           at: database,
+                                           consequence: followUp)
+                    return
+                }
+                
+                // if not handled...
+                NotificationCenter.default.post(name: MCNotification.subscription, object: error)
+            } else {
+                if let action = followUp { action() }
             }
-            
-            // if not handled...
-            NotificationCenter.default.post(name: MCNotification.subscription, object: error)
-            
-            // Prevents infinite retries...
-            guard left > 0 else { return }
-            
-            // TODO: depending on error type and count retry
-            let _ = setupListener(for: type,
-                                  change: trigger,
-                                  at: database,
-                                  withTries: left,
-                                  consequence: followUp)
-        } else {
-            if let action = followUp { action() }
         }
+        
+        id = subsciption.subscriptionID
     }
     
-    return subsciption.subscriptionID
-}
+    // !!
+    public func end(subscriptionID: String? = nil, at database: DatabaseType = .publicDB) {
 
-// !!
-public func disableListener(subscriptionID id: String,
-                            withTries count: Int = 2,
-                            at database: DatabaseType = .publicDB) {
-    let left: Int
-    count < 2 ? (left = count - 1) : (left = 2)
-
-    database.db.delete(withSubscriptionID: id) { possibleID, possibleError in
+        // This loads id with either subscriptionID, self.id, or exits if both are nil.
+        guard let id = subscriptionID ?? id else { return }
+        
+        database.db.delete(withSubscriptionID: id) { possibleID, possibleError in
 print("** disabling subscription")
-        if let error = possibleError {
+            if let error = possibleError {
 print("** error disabling: \(error)")
-            NotificationCenter.default.post(name: MCNotification.subscription, object: error)
-            
-            // Prevents infinite retries...
-            guard left > 0 else { return }
-            
-            disableListener(subscriptionID: id, withTries: left, at: database)
+                NotificationCenter.default.post(name: MCNotification.subscription, object: error)
+            }
         }
     }
 }
