4f7c83e07948aac808831771f89dd18a06899e2b
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-22 18:13:58 -0800

    setup tests for NotificationReader and extracted MCNotificationReader

--- MagicCloudTests/NotificationReaderTests.swift
+++ MagicCloudTests/NotificationReaderTests.swift
@@ -9,11 +9,21 @@
 import XCTest
 import CloudKit
 
+// !! CAUTION: These tests assume MCNotificationReader is implemented in app delegate.
 class NotificationReaderTests: XCTestCase {
     
     // MARK: - Properties
     
     var mockRec: MockReceiver?
+    
+    var mockRecordables: [MockRecordable] {
+        var array = [MockRecordable]()
+        
+        array.append(MockRecordable(created: Date.distantPast))
+        array.append(MockRecordable(created: Date.distantFuture))
+        
+        return array
+    }
     
     // MARK: - Functions
     
@@ -23,18 +33,44 @@ class NotificationReaderTests: XCTestCase {
     }
     
     override func tearDown() {
-        super.tearDown()
+        super.tearDown()        
         mockRec = nil
     }
 
+    func prepareDatabase() -> Int {
+        let op = Upload(mockRecordables, from: mockRec!, to: .publicDB)
+        let pause = Pause(seconds: 3)
+        pause.addDependency(op)
+        pause.completionBlock = { print("finished prep pause") }
+        OperationQueue().addOperation(pause)
+        OperationQueue().addOperation(op)
+        
+        pause.waitUntilFinished()
+        return 0
+    }
+    
+    func cleanUpDatabase() -> Int {
+        let op = Delete(mockRecordables, of: mockRec!, from: .publicDB)
+        let pause = Pause(seconds: 2)
+        pause.addDependency(op)
+        pause.completionBlock = { print("finished cleanUp pause") }
+        OperationQueue().addOperation(pause)
+        OperationQueue().addOperation(op)
+        
+        pause.waitUntilFinished()
+        return 0
+    }
+    
     // MARK: - Functions: Tests
     
     func testNotificationReceiverCanConvertRemoteNotificationToLocal() {
         mockRec?.subscribeToChanges(on: .publicDB)
         
+        let _ = prepareDatabase()
+        XCTAssert(mockRec?.recordables.count != 0)
         
-        
-        
+        let _ = cleanUpDatabase()
+        XCTAssert(mockRec?.recordables.count == 0)
     }
 }
 
@@ -45,7 +81,7 @@ protocol NotificationReader {
 struct MCNotificationReader: NotificationReader {
 
     // !!
-    static func handle(_ error: CKError, for info: [AnyHashable: Any]) {
+    fileprivate static func handle(_ error: CKError, for info: [AnyHashable: Any]) {
         switch error.code {
         case .networkUnavailable, .networkFailure,
              .serviceUnavailable, .requestRateLimited,
