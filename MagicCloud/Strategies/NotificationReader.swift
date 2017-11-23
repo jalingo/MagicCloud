46f1efaad161841c24a7a2956b641fa1da3355a7
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-22 18:18:24 -0800

    pulled out NotificationReader from tests to framework.

---
+++ MagicCloud/Strategies/NotificationReader.swift
@@ -0,0 +1,61 @@
+//
+//  NotificationReader.swift
+//  MagicCloud
+//
+//  Created by James Lingo on 11/22/17.
+//  Copyright © 2017 Escape Chaos. All rights reserved.
+//
+
+import CloudKit
+
+struct NotificationReader {
+    
+    // !!
+    fileprivate static func handle(_ error: CKError, for info: [AnyHashable: Any]) {
+        switch error.code {
+        case .networkUnavailable, .networkFailure,
+             .serviceUnavailable, .requestRateLimited,
+             .resultsTruncated,   .zoneBusy:
+            if let retryAfterValue = error.userInfo[CKErrorRetryAfterKey] as? TimeInterval {
+                let pause = Pause(seconds: retryAfterValue)
+                pause.completionBlock = { MCNotificationReader.createLocal(from: info) }
+                pause.start()
+            }
+        case .unknownItem: break
+        case .partialFailure:
+            if let dictionary = error.userInfo[CKPartialErrorsByItemIDKey] as? NSDictionary {
+                for entry in dictionary {
+                    if let partialError = entry.value as? CKError { handle(partialError, for: info) }
+                }
+            }
+        default:
+            let name = Notification.Name(MCNotification.error(error).toString())
+            NotificationCenter.default.post(name: name, object: error)
+        }
+    }
+    
+    // !!
+    static func createLocal(from info: [AnyHashable: Any]) {
+        // Pull a CKNotification from userInfo, containing triggers and ckrecordID
+        let remote = CKQueryNotification(fromRemoteNotificationDictionary: info)
+        let database = DatabaseType.from(scope: remote.databaseScope)
+        
+        guard let id = remote.recordID else { return }
+        
+        let op = CKFetchRecordsOperation(recordIDs: [id])
+        op.fetchRecordsCompletionBlock = { possibleResults, possibleError in
+            if let error = possibleError as? CKError { MCNotificationReader.handle(error, for: info) }
+            
+            guard let results = possibleResults else { return }
+            
+            if let type = results[id]?.recordType {
+                let change = MCNotification.changeNoticed(forType: type, at: database)
+                let local = Notification.Name(change.toString())
+                
+                NotificationCenter.default.post(name: local, object: change)
+            }
+        }
+        
+        database.db.add(op)
+    }
+}
