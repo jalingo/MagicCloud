e6a83dbe0258f74ccb9e9191dc0afb128323b576
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-27 12:21:16 -0800

    minor commenting

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-print("** didFinishLaunchingWithOptions")
+print("** didFinishLaunchingWithOptions @ TestApp delegate")
         application.registerForRemoteNotifications()
         
         return true
