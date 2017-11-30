27a8517a41a5f932876a2e334ea723a24c1bb881
Jimmy Lingo <jalingo@berkeley.edu>
2017-11-29 23:52:43 -0800

    renamed / minor comments

--- MagicCloud/Strategies/MCNotificationConverter.swift
+++ MagicCloud/Strategies/MCNotificationConverter.swift
@@ -9,15 +9,17 @@
 import CloudKit
 
 /// Conforming to this protocol allows remote notifications to be converted to local notifications and triggers behavior.
-public protocol NotificationConverter {
+public protocol MCNotificationConverter {
     
     /// This method creates a local notification from remote notifiation's userInfo, if intended for MagicCloud.
     /// Implementation for this method should not be overwritten.
     func convertToLocal(from info: [AnyHashable: Any])
 }
 
-public extension NotificationConverter {
+public extension MCNotificationConverter {
     
+    /// This method creates a local notification from remote notifiation's userInfo, if intended for MagicCloud.
+    /// Implementation for this method should not be overwritten.
     public func convertToLocal(from info: [AnyHashable: Any]) {
         let notification = CKQueryNotification(fromRemoteNotificationDictionary: info)
         
