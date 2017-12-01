eea648be92240f0e28e2b99fa33b18dabfd1fe97
Jimmy Lingo <jalingo@berkeley.edu>
2017-12-01 00:31:50 -0800

    adding type implementation

--- MagicCloud/Strategies/GetUserRecord.swift
+++ MagicCloud/Strategies/GetUserRecord.swift
@@ -28,3 +28,37 @@ public func getCurrentUserRecord() -> CKRecordID? {
     group.wait()
     return result
 }
+
+/// This struct contains a static var (singleton) which accesses USER's iCloud CKRecordID.
+public struct MCUserRecord {
+    
+    /**
+        This read-only, computed property should be called async from main thread because it calls to remote database before returning value. If successful returns the User's CloudKit CKRecordID, otherwise returns nil.
+     */
+    static var singleton: CKRecordID? {
+        var result: CKRecordID?
+
+        let group = DispatchGroup()
+        group.enter()
+        
+        CKContainer.default().fetchUserRecordID { possibleID, possibleError in
+            if let error = possibleError as? CKError { MCFetchRecordIDError.handle(error) }
+            
+            if let id = possibleID { result = id }
+            group.leave()
+        }
+        
+        group.wait()
+        return result
+    }
+}
+
+public struct MCFetchRecordIDError {
+    
+    static func handle(_ error: CKError) {
+
+        // If not handled...
+        let name = Notification.Name(MCNotification.error(error).toString())
+        NotificationCenter.default.post(name: name, object: error)
+    }
+}
