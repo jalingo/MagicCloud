547777e1206a0905a679a7cc11e2aee7f0d3eeb1
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-10 13:06:11 -0800

    added Bool return to convertToLocal

--- MagicCloud/Protocols/API/MCNotificationConverter.swift
+++ MagicCloud/Protocols/API/MCNotificationConverter.swift
@@ -11,12 +11,7 @@ import CloudKit
 // MARK: Protocol
 
 /// Conforming to this protocol allows remote notifications to be converted to local notifications and triggers behavior.
-public protocol MCNotificationConverter {
-    
-    /// This method creates a local notification from remote notifiation's userInfo, if intended for MagicCloud.
-    /// Implementation for this method should not be overwritten.
-    func convertToLocal(from info: [AnyHashable: Any])
-}
+public protocol MCNotificationConverter { }
 
 // MARK: - Extension
 
@@ -25,14 +20,17 @@ public extension MCNotificationConverter {
     
     /// This method creates a local notification from remote notifiation's userInfo, if intended for MagicCloud.
     /// Implementation for this method should not be overwritten.
-    public func convertToLocal(from info: [AnyHashable: Any]) {
+    /// - Returns: If successful, then true. Else, false.
+    public func convertToLocal(from info: [AnyHashable: Any]) -> Bool {
         let notification = CKQueryNotification(fromRemoteNotificationDictionary: info)
         
         if let type = notification.alertLocalizationKey {
             let name = Notification.Name(type)
             NotificationCenter.default.post(name: name, object: nil, userInfo: info)
+            return true
         } else {
             print("** userInfo: \(info.values)")
+            return false
         }
     }
 }
