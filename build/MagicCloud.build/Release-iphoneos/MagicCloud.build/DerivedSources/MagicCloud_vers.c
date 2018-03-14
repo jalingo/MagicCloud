3990edc6908ac9095fb07a8641e373e02191b103
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-14 00:53:08 -0700

    rm build directory

--- build/MagicCloud.build/Release-iphoneos/MagicCloud.build/DerivedSources/MagicCloud_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MagicCloudVersionString[];
- extern const double MagicCloudVersionNumber;
-
- const unsigned char MagicCloudVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MagicCloud  PROJECT:MagicCloud-1" "\n";
- const double MagicCloudVersionNumber __attribute__ ((used)) = (double)1.;
