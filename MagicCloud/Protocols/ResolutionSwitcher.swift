b7541b9134f12ee829e4b59e6efd44470fca96f9
Jimmy Lingo <jalingo@berkeley.edu>
2018-03-27 00:40:30 -0700

    abstracted error cases

--- MagicCloud/Protocols/ResolutionSwitcher.swift
+++ MagicCloud/Protocols/ResolutionSwitcher.swift
@@ -9,7 +9,7 @@
 import CloudKit
 
 /// Types conforming to the protocol can call the generic `resolve:error:in:from:to:withPolicy:whileIgnoringUnknowns:unknownCustomAction` method that selects resolution strategy based on error code and then executes said resolution.
-protocol ResolutionSwitcher: MCRetrier, BatchErrorResolver { }
+protocol ResolutionSwitcher: RetriableErrorResolver, BatchErrorResolver, VersionConflictResolver { }
 
 extension ResolutionSwitcher where Self: Operation & MCDatabaseModifier {
     
@@ -26,60 +26,25 @@ extension ResolutionSwitcher where Self: Operation & MCDatabaseModifier {
     ///     - ignoreUnknownItemCustomAction: When not nil, this closure will be executed if an unknown item situation occurs. If set, `ignoreUnknownItem` will be treated as true, regardless of passed argument.
     func resolve<R: MCMirrorAbstraction>(_ error: CKError, in originatingOp: Operation, with recordables: [R.type], from receiver: R, to database: MCDatabase, withPolicy conflictResolutionPolicy: CKRecordSavePolicy, whileIgnoringUnknowns ignoreUnknownItem: Bool, unknownCustomAction ignoreUnknownItemCustomAction: OptionalClosure = nil) {
         
-        // After the following switch identifies the type of error, variable will contain response.
-        var resolvingOperation: Operation?
-        
         switch error.code {
             
         // This error occurs when record's change tag indicates a version conflict (modify operations).
-        case .serverRecordChanged:
-            resolvingOperation = VersionConflict(rec: receiver,
-                                                 error: error,
-                                                 target: database,
-                                                 policy: conflictResolutionPolicy,
-                                                 instances: recordables,
-                                                 completionBlock: completionBlock)
-            completionBlock = nil
-            
+        case .serverRecordChanged: resolveVersionConflict(error, accordingTo: conflictResolutionPolicy)
+
         // These errors occur when a batch of requests fails or partially fails (batch operations).
         case .limitExceeded, .batchRequestFailed, .partialFailure: resolveBatch(error, in: originatingOp)
             
         // These errors occur as a result of environmental factors, and originating operation should be retried after a set amount of time.
-        case .networkUnavailable, .networkFailure, .serviceUnavailable, .requestRateLimited, .zoneBusy:
-            guard let retryAfterValue = error.userInfo[CKErrorRetryAfterKey] as? TimeInterval else { return }
+        case .networkUnavailable, .networkFailure, .serviceUnavailable, .requestRateLimited, .zoneBusy: resolveRetriable(error, in: originatingOp)
             
-            if isCancelled { return }
-            
-            let q = DispatchQueue(label: retriableLabel)
-            if let op = replicate(originatingOp, with: receiver) {
-                
-                if isCancelled { return }
-                
-                q.asyncAfter(deadline: .now() + retryAfterValue) {
-                    if self.isCancelled { return }
-                    
-                    if let cloudOp = op as? CKDatabaseOperation {
-                        database.db.add(cloudOp)
-                    } else {
-                        OperationQueue().addOperation(op)
-                    }
-                }
-            }
-            
-            // These errors occur when CloudKit has a problem with a CKSharedDatabase operation.    // <-- Not currently supported but left here as a reminder for future versions.
-            //        case .alreadyShared, .tooManyParticipants:
+        // These errors occur when CloudKit has a problem with a CKSharedDatabase operation.    // <-- Not currently supported but left here as a reminder for future versions.
+//        case .alreadyShared, .tooManyParticipants:
             
         // This case allows .unknownItem to be ignored (query / fetch / modify operations).
-        case .unknownItem where ignoreUnknownItem:
-            if let block = ignoreUnknownItemCustomAction { block() }
+        case .unknownItem where ignoreUnknownItem: if let block = ignoreUnknownItemCustomAction { block() }
             
         // These fatal errors do not require any further handling.
         default: break
         }
-        
-        if isCancelled { return }
-        
-        // After resolution determined in previous switch statement, resolution is initiated here.
-        if let op = resolvingOperation { OperationQueue().addOperation(op) }
     }
 }
